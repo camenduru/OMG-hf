155dd80b8e9105155da75f31086024a3344f29b4
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 14:14:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
 os.chdir(cwd1)
 os.system("make setup")
 os.system("python app.py")
-os.chdir(cwd0)
+print(os.system(f"cd /home/user/app"))
 
 sys.path.append('./')
 import gradio as gr
