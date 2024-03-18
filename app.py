9f394466b76048c99e4efe653b23bf96b8f43df1
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 15:03:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import spaces
 from diffusers import DiffusionPipeline
+import torch
+import gradio as gr
 
 pipe = DiffusionPipeline.from_pretrained("Fucius/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16")
 pipe.to('cuda')
