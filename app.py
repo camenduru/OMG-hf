000a20511c681c8027f91ff2617845e18f455af5
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 17:10:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ import spaces
 import sys
 import os
 
-# os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
-# cwd0 = os.getcwd()
-# cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
-# os.chdir(cwd1)
-# os.system("make setup")
-# os.system(f"cd /home/user/app")
-
 sys.path.append('./')
 import gradio as gr
 import random
