0b5aea76fb12f41e460c3ad7f0a03603208ec911
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 16:17:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 
-
+print(os.getcwd())
 # os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
 # cwd0 = os.getcwd()
 # cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
