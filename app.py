31595077777fa25621bba9245c0fb57b2264297c
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 17:25:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import os
 
 
 
-# os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
-# cwd0 = os.getcwd()
-# cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
-# os.chdir(cwd1)
-# os.system("make setup")
-# os.system(f"cd /home/user/app")
+os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
+cwd0 = os.getcwd()
+cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
+os.chdir(cwd1)
+os.system("make setup")
+os.system(f"cd /home/user/app")
 
 sys.path.append('./')
 import gradio as gr
