59def2a91a27cee8a5b6ec9e30ed6bd72baba38b
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 17:23:04 +0000

    Delete inference

--- inference/core/entities/responses/workflows.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Dict
-
-from pydantic import BaseModel, Field
-
-
-class WorkflowInferenceResponse(BaseModel):
-    outputs: Dict[str, Any] = Field(
-        description="Dictionary with keys defined in workflow output and serialised values"
-    )
