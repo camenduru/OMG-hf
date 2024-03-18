4fa2f96febdb102e68ee5ffcea962851d67cdb29
Kong <Fucius@users.noreply.huggingface.co>
2024-03-18 15:54:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import spaces
 import gradio as gr
 import torch
 
-pipe = DiffusionPipeline.from_pretrained(...)
+pipe = DiffusionPipeline.from_pretrained("Fucius/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16")
 pipe.to('cuda')
 
 @spaces.GPU
 def generate(prompt):
-    
     return pipe(prompt).images
 
 gr.Interface(
