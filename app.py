7b43b383fce273dec7021830753ccc5cb66eae08
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 06:35:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import torch
 pipe = DiffusionPipeline.from_pretrained("Fucius/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16")
 pipe.to('cuda')
 
+
 @spaces.GPU
 def generate(prompt):
     return pipe(prompt).images
