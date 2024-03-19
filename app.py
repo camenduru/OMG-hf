261c227e721b21ec8c457101e6b2a91012bd273a
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 11:52:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ from diffusers.utils import load_image
 import cv2
 from PIL import Image, ImageOps
 from transformers import DPTFeatureExtractor, DPTForDepthEstimation
-# from controlnet_aux import OpenposeDetector
+from controlnet_aux import OpenposeDetector
 # from controlnet_aux.open_pose.body import Body
 
 
 from inference.models import YOLOWorld
-# from src.efficientvit.models.efficientvit.sam import EfficientViTSamPredictor
+from src.efficientvit.models.efficientvit.sam import EfficientViTSamPredictor
 from src.efficientvit.sam_model_zoo import create_sam_model
 import supervision as sv
 
