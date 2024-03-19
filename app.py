4104006f50e825f4e5e05861f33ca6aacdd70a95
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 15:55:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -367,8 +367,7 @@ def main(device, segment_type):
         image = Image.fromarray((image * 255.0).clip(0, 255).astype(np.uint8))
         return image
 
-    # @spaces.GPU(duration=200)
-    @spaces.GPU
+    @spaces.GPU(duration=200)
     def generate_image(prompt1, negative_prompt, man, woman, resolution, local_prompt1, local_prompt2, seed, condition, condition_img1, style):
         # try:
         path1 = lorapath_man[man]
