47d21ecf7ba1eeaedd5818f5ed7168ba29dcf63c
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 08:02:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 
 
 
-# os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
-# cwd0 = os.getcwd()
-# cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
-# os.chdir(cwd1)
-# os.system("make setup")
-# os.system(f"cd /home/user/app")
+os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
+cwd0 = os.getcwd()
+cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
+os.chdir(cwd1)
+os.system("make setup")
+os.chdir(f"cd /home/user/app")
 
-os.system(f"pip install inference[yolo-world]==0.9.13")
-print("install inference-yolo-world")
+# os.system(f"pip install inference[yolo-world]==0.9.13")
+# print("install inference-yolo-world")
 os.system(f"python app2.py")
