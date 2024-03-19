df4d1c61eafdeb87fd1dd3cced66200f4ea889ba
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 08:07:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-import spaces
-import sys
 import os
-
-
 
 os.system(f"git clone https://github.com/Curt-Park/yolo-world-with-efficientvit-sam.git")
 cwd0 = os.getcwd()
 cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
 os.chdir(cwd1)
 os.system("make setup")
-os.chdir(f"/home/user/app")
-
-# os.system(f"pip install inference[yolo-world]==0.9.13")
-# print("install inference-yolo-world")
-os.system(f"python app2.py")
+os.system("python app.py")
