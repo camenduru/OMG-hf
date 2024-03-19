42a6bb3a2a997c7461520ed16700fcbd4a09b467
Kong <Fucius@users.noreply.huggingface.co>
2024-03-19 08:05:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ cwd0 = os.getcwd()
 cwd1 = os.path.join(cwd0, "yolo-world-with-efficientvit-sam")
 os.chdir(cwd1)
 os.system("make setup")
-os.chdir(f"cd /home/user/app")
+os.chdir(f"/home/user/app")
 
 # os.system(f"pip install inference[yolo-world]==0.9.13")
 # print("install inference-yolo-world")
